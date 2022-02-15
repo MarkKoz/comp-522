b7c9338f0f991e7cfca9c2f73d908ac8bdebe4ba
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-02-14 19:54:20 -0800

    Rename frequency to period

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 const uint8_t BUTTON_PIN = 2;
 const uint8_t LED_PIN = 13;
 
-const int16_t MIN_FREQ = 1000; // 1 Hz
-const int16_t MAX_FREQ = MIN_FREQ / 8; // 8 Hz
+const int16_t MIN_PERIOD = 1000; // 1 Hz
+const int16_t MAX_PERIOD = MIN_PERIOD / 8; // 8 Hz
 
 void setup()
 {
@@ -16,18 +16,18 @@ void loop()
 {
     static int button_state_prev = LOW;
     static uint8_t led_state = LOW;
-    static int16_t frequency = MIN_FREQ;
+    static int16_t period = MIN_PERIOD;
 
     int button_state = digitalRead(BUTTON_PIN);
     if (button_state == HIGH && button_state != button_state_prev) {
-        // Frequency needs to be signed because MIN_FREQ > MAX_FREQ.
-        // Thus, subtracting MIN_FREQ makes numbers go negative.
-        frequency = ((frequency / 2 - MIN_FREQ) % (MAX_FREQ / 2 - MIN_FREQ)) + MIN_FREQ;
+        // Frequency needs to be signed because MIN_PERIOD > MAX_PERIOD.
+        // Thus, subtracting MIN_PERIOD makes numbers go negative.
+        period = ((period / 2 - MIN_PERIOD) % (MAX_PERIOD / 2 - MIN_PERIOD)) + MIN_PERIOD;
     }
     button_state_prev = button_state;
 
     led_state = ~led_state;
     digitalWrite(LED_PIN, led_state);
 
-    delay(static_cast<uint16_t>(frequency));
+    delay(static_cast<uint16_t>(period));
 }
