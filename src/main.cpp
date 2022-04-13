0d13ef2b5882d0126934c268c619819c88c15b47
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-04-13 12:02:00 -0700

    Implement basic rotation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <Arduino.h>
+#include <Stepper.h>
 
-void setup() { }
+// Based on information from https://lastminuteengineers.com/28byj48-stepper-motor-arduino-tutorial/
+const uint16_t STEPS_PER_REVOLUTION = 2038;
 
-void loop() { }
+// Adjustable range is 0-15 RPM.
+const uint8_t SPEEDS[4] = {2, 7, 11, 15};
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    static uint8_t speed_index = 0;
+    static Stepper stepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11);
+
+    stepper.setSpeed(SPEEDS[speed_index]);
+    stepper.step(1);
+}
